c6f239357a97e6dd6853649915a3a96837056fe0
amy17519 <amy17519@gmail.com>
2022-04-23 18:45:33 -0700

    add conda

--- D2.sh
+++ D2.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate ling573-2022-spring
 python src/baseline/test_models.py
 LC_ALL=en_US.UTF-8 python src/model_runner.py convert /home2/droesl/573/test_model.pth --log-dir .logging --experiment-name default --experiment-version 0 -c runner_config.json
 LC_ALL=en_US.UTF-8 python src/model_runner.py test data/balanced_validation_En.csv outputs/D2/bert_tweet/pred_output.csv results/D2/bert_tweet/metrics.txt --log-dir .logging/ --experiment-name default --experiment-version 0
