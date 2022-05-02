1c655d6cc2efe44404ee7a6a6d1c737573a10af5
David Roesler <roeslerdavidthomas@gmail.com>
2022-05-02 16:43:35 -0700

    Added bertweet-large model

--- src/models.py
+++ src/models.py
@@ -2,10 +2,107 @@ import os
 import torch
 import torch.nn as nn
 
+from nltk.tokenize import TweetTokenizer
+from emoji import demojize
+
 from functools import partial
 from transformers import AutoModel, AutoTokenizer
 
 MAX_LEN = 100
+
+
+def preprocessing_for_bertweetlarge(tokenizer, data):
+    """Perform required preprocessing steps for pretrained BERTweet-large.
+    @param    data (np.array): Array of texts to be processed.
+    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
+    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+                  tokens should be attended to by the model.
+    """
+
+    # nltk tweettokenizer for tweet normalization
+    bertweet_preprocessing_tokenizer = TweetTokenizer()
+
+    # define tweet normalization functions
+    def normalizeToken(token):
+        lowercased_token = token.lower()
+        if token.startswith("@"):
+            return "@USER"
+        elif lowercased_token.startswith("http") or lowercased_token.startswith("www"):
+            return "HTTPURL"
+        elif len(token) == 1:
+            return demojize(token)
+        else:
+            if token == "’":
+                return "'"
+            elif token == "…":
+                return "..."
+            else:
+                return token
+
+    def normalizeTweet(tweet):
+        tokens = bertweet_preprocessing_tokenizer.tokenize(tweet.replace("’", "'").replace("…", "..."))
+        normTweet = " ".join([normalizeToken(token) for token in tokens])
+
+        normTweet = (
+            normTweet.replace("cannot ", "can not ")
+            .replace("n't ", " n't ")
+            .replace("n 't ", " n't ")
+            .replace("ca n't", "can't")
+            .replace("ai n't", "ain't")
+        )
+        normTweet = (
+            normTweet.replace("'m ", " 'm ")
+            .replace("'re ", " 're ")
+            .replace("'s ", " 's ")
+            .replace("'ll ", " 'll ")
+            .replace("'d ", " 'd ")
+            .replace("'ve ", " 've ")
+        )
+        normTweet = (
+            normTweet.replace(" p . m .", "  p.m.")
+            .replace(" p . m ", " p.m ")
+            .replace(" a . m .", " a.m.")
+            .replace(" a . m ", " a.m ")
+        )
+
+        return " ".join(normTweet.split())
+
+
+    # Create empty lists to store outputs
+    input_ids = []
+    attention_masks = []
+    
+    # For every sentence...
+    for sent in data:
+
+        # normalize tweet 
+        sent = normalizeTweet(sent)
+
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_sent = tokenizer.encode_plus(
+            text=sent,
+            add_special_tokens=True,  # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,  # Max length to truncate/pad
+            pad_to_max_length=True,  # Pad sentence to max length
+            # return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True  # Return attention mask
+        )
+
+        # Add the outputs to the lists
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
 
 
 def preprocessing_for_bert(tokenizer, data):
@@ -169,8 +266,8 @@ class BertSmallClassifier(nn.Module):
         return partial(preprocessing_for_bert, tokenizer)
 
 
-class D2Model(nn.Module):
-    """David's Model for D2. TestModel
+class BertweetLargeClassifier(nn.Module):
+    """Bertweet-large Model for Classification Tasks.
     """
 
     def __init__(self, freeze_bert=False):
@@ -179,14 +276,12 @@ class D2Model(nn.Module):
         @param    classifier: a torch.nn.Module classifier
         @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
         """
-        super().__init__()
+        super(BertweetLargeClassifier, self).__init__()
         # Specify hidden size of BERT, hidden size of our classifier, and number of labels
-        D_in, H, D_out = 128, 50, 2
+        D_in, H, D_out = 1024, 100, 2
 
-        # Instantiate BERT model
-        # self.bert = RobertaModel.from_pretrained('roberta-base')
-        # self.bert = BertModel.from_pretrained('bert-base-cased')
-        self.bert = AutoModel.from_pretrained("")
+        # Instantiate model
+        self.bert = AutoModel.from_pretrained("vinai/bertweet-large")
 
         # Instantiate an one-layer feed-forward classifier
         self.classifier = nn.Sequential(
@@ -222,3 +317,8 @@ class D2Model(nn.Module):
         logits = self.classifier(last_hidden_state_cls)
 
         return logits
+
+    @staticmethod
+    def get_preprocessor():
+        tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-large", normalization=False, use_fast=False)
+        return partial(preprocessing_for_bertweetlarge, tokenizer)
