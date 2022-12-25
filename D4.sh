e14e434c3ed8492af45f31a84ce9965c07b8272b
David Roesler <roeslerdavidthomas@gmail.com>
2022-12-24 16:38:47 -0800

    Update D4.sh

--- D4.sh
+++ D4.sh
@@ -11,5 +11,5 @@ LC_ALL=en_US.UTF-8 python src/model_runner.py test data/task_A_En_test_column_re
 python src/get_thresholded_predictions.py
 
 # run adaptation task predictions
-LC_ALL=en_US.UTF-8 python src/model_runner.py test data/balanced_validation_Ar.csv  outputs/D4/adaptation/devtest/ar_pred_output.csv  results/D4/adaptation/devtest/ar_metrics.txt --model-class CamelbertMixClassifier  --log-dir /home2/diazhang/LING573/logging/ --experiment-name diazhang --experiment-version 1 -c runner_config.json
-LC_ALL=en_US.UTF-8 python src/model_runner.py test data/task_A_Ar_test_column_renamed.csv  outputs/D4/adaptation/evaltest/ar_pred_output.csv  results/D4/adaptation/evaltest/ar_metrics.txt --model-class CamelbertMixClassifier  --log-dir /home2/diazhang/LING573/logging/ --experiment-name diazhang --experiment-version 1 -c runner_config.json
+LC_ALL=en_US.UTF-8 python src/model_runner.py test data/balanced_validation_Ar.csv  outputs/D4/adaptation/devtest/ar_pred_output.csv  results/D4/adaptation/devtest/ar_metrics.txt --model-class CamelbertMixClassifier  --log-dir .logging --experiment-name diazhang --experiment-version 1 -c runner_config.json
+LC_ALL=en_US.UTF-8 python src/model_runner.py test data/task_A_Ar_test_column_renamed.csv  outputs/D4/adaptation/evaltest/ar_pred_output.csv  results/D4/adaptation/evaltest/ar_metrics.txt --model-class CamelbertMixClassifier  --log-dir .logging --experiment-name diazhang --experiment-version 1 -c runner_config.json
